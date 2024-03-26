35c108ecba02a840f12b153464918792c314dbb9
Martin Wittmann <me@martinwittmann.at>
2024-03-26 18:52:47 +0100

    Fixed update nginx config scripts + .env files for project templates.

--- project-templates/dummy/.project/scripts/update_nginx_config.sh
+++ project-templates/dummy/.project/scripts/update_nginx_config.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-project_run_global_script "create_nginx_config_for_project" "$PROJECT_NAME" "$PROJECT_PROXY_PROJECT_NAME:$PROJECT_NGINX_TEMPLATE" "$PROJECT_PROXY_PROJECT_NAME" 1
+project_run_global_script "create_nginx_config_for_project" "$PROJECT_NAME" "$PROJECT_NGINX_TEMPLATE" "$PROJECT_PROXY_PROJECT_NAME" 1
 
 project_run_global_script "create_nginx_log_files"
